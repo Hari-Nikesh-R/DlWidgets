955d41d0ca097b0c1a268a1b650dcdf95dbe0bf6
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2023-05-23 00:15:14 +0530

    Created Dls for Button with minimal functionality.

--- lib/src/dl_button.dart
+++ lib/src/dl_button.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
+@immutable
 class DlButton extends StatelessWidget {
-  const DlButton({Key? key}) : super(key: key);
+   const DlButton(
+      {super.key, required this.buttonName, required this.size, required this.buttonColor, required this.onPressed,  this.onLongPressed});
+
+  final String buttonName;
+  final ButtonSize? size;
+  final Color buttonColor;
+  final VoidCallback onPressed;
+  final VoidCallback? onLongPressed;
+
+  double selectSize(BuildContext context) {
+    if (size == ButtonSize.small) {
+      return MediaQuery
+          .of(context)
+          .size
+          .width / 20;
+    }
+    else if (size == ButtonSize.medium) {
+      return MediaQuery
+          .of(context)
+          .size
+          .width / 14;
+    }
+    else if (size == ButtonSize.large) {
+      return MediaQuery
+          .of(context)
+          .size
+          .width / 8;
+    }
+    return MediaQuery
+        .of(context)
+        .size
+        .width / 9;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor, elevation: 8, shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0)
+      )// Background color
+      ),
+      onLongPress: onLongPressed,
+      child: Padding(padding: EdgeInsets.symmetric(
+          horizontal: selectSize(context), vertical: 10),
+          child: Text(buttonName, style:
+          const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.normal,
+          ))
+      ),
+    );
   }
+}
+
+enum ButtonSize{
+  large, medium, small
 }
