ff2a1814407d460f2a2a2ecbf531479fd6139237
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2023-05-26 00:24:07 +0530

    Completed the flow for textFormField.

--- lib/src/dl_textformfield.dart
+++ lib/src/dl_textformfield.dart
@@ -1,31 +1,65 @@
 import 'package:flutter/material.dart';
 
-class DlTextFormField extends StatelessWidget {
-  const DlTextFormField({super.key, this.activeColor, this.trailingIcon, required this.hintText});
+class DlTextFormField extends StatefulWidget {
+  const DlTextFormField({super.key, this.activeColor, this.trailingIcon, this.isPassword, this.onChanged, required this.textFieldController, this.labelText});
   final Color? activeColor;
   final Widget? trailingIcon;
-  final String hintText;
+  final bool? isPassword;
+  final TextEditingController textFieldController;
+  final ValueChanged<String>? onChanged;
+  final String? labelText;
 
-  //Under constuction
+  @override
+  State<DlTextFormField> createState() => _DlTextFormFieldState();
+}
+
+class _DlTextFormFieldState extends State<DlTextFormField> {
+
+  bool isObscured = false;
+
+  @override
+  void initState() {
+    isObscured = widget.isPassword??false;
+    super.initState();
+  }
+
   InputBorder? getOutlineBorder(Color borderColor){
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(12),
-    borderSide: BorderSide(
-    color: borderColor
-    ));
+        borderSide: BorderSide(
+            color: borderColor
+        ));
   }
 
+  Widget? passwordIcon(){
+    return IconButton(icon: !isObscured ? const Icon(Icons.visibility, color: Colors.black) :
+    const Icon(Icons.visibility_off, color: Colors.black,),
+        onPressed: (){
+          setState(() {
+            isObscured = !isObscured;
+          });
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: widget.textFieldController,
+      obscureText:isObscured,
+      maxLines: 1,
+      onChanged: widget.onChanged,
+      autocorrect: widget.isPassword??false,
       decoration: InputDecoration(
-        hintText: hintText,
-        border: getOutlineBorder(activeColor??Colors.blue),
+          fillColor: Colors.white,
+          filled: true,
+          suffixIcon: widget.isPassword??false ? passwordIcon() : null,
+          border: getOutlineBorder(widget.activeColor??Colors.blue),
           errorBorder: getOutlineBorder(Colors.red),
-          enabledBorder: getOutlineBorder(activeColor??Colors.blue),
-        icon: trailingIcon
-        ),
+          enabledBorder: getOutlineBorder(widget.activeColor??Colors.blue),
+          icon: widget.trailingIcon,
+          labelText: widget.labelText
+      ),
     );
   }
 }
+
